80e9728eb0b3e970907431ca07a7d1f6a00ce6a2
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-11-09 18:33:13 -0700

    add challenge 17

---
+++ javascript/401/Challenge-17/module/BinaryTree.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+    this.newMax = 0;
+  }
+  breadthFirst(tree){
+    if(!tree.root){
+      return 'There is no tree :D';
+    }
+    console.log('we in here');
+    this.array = [];
+    this.array.push(tree.root);
+    this.output = [];
+    while(this.array.length){
+      const current = this.array.shift();
+      
+      if(current.left){
+        this.array.push(current.left);
+      }
+      if(current.right){
+        this.array.push(current.right);
+      }
+      this.output.push(current.value);
+    }
+    return this.output;
+  }
+}
+
+module.exports = BinaryTree;
