27fc38cdacc61fe4816def617a46db1e5c3ce8dc
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-12-01 17:21:56 -0700

    added hashmap for repeat

--- javascript/401/Challenge-27/module/mergeSort.js
+++ javascript/401/Challenge-27/module/mergeSort.js
@@ -1,82 +1,50 @@
 'use strict';
 
+const mergeSort = (arr) => {
+  if (arr.length === 0) {
+    return 'please enter an array with integers';
+  }
 
-function mergeSort(arr){
-
-        const n = arr.length;
-
-        if(n > 1){
-            const mid = n/2;
-            const left = arr[0, mid];
-            const right = arr[mid, n];
-
-            mergeSort(left);
-            mergeSort(right);
-            merge(left, right, arr)
-        }
-
+  let n = arr.length;
+  if (n > 1) {
+    let mid = Math.floor(n / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, arr);
+  }
+  return arr;
 };
 
-function merge(left, right, arr){
+const merge = (left, right, arr) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
 
-    const i = 0;
-    const j = 0;
-    const k = 0;
-
-    while(i < left.length && j < right.length){
-        if(left[i] <= right[j]){
-            arr[k] = left[i];
-            i++;
-        } else {
-            arr[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-    if(i = left.length){
-        for(var a = 0; a < arr.length; a++ ){
-            right.push(arr[a]);
-        }
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
     } else {
-        for(var a = 0; a < arr.length; a++ ){
-            left.push(arr[a]);
-        }
+      arr[k] = right[j];
+      j++;
     }
-
-}
+    k++;
+  }
+  if (i === left.length) {
+    while (k < arr.length) {
+      arr[k] = right[j];
+      j++;
+      k++;
+    }
+  } else {
+    while (k < arr.length) {
+      arr[k] = left[i];
+      i++;
+      k++;
+    }
+  }
+};
 
 module.exports = mergeSort;
-
-ALGORITHM Mergesort(arr)
-    DECLARE n <-- arr.length
-
-    if n > 1
-      DECLARE mid <-- n/2
-      DECLARE left <-- arr[0...mid]
-      DECLARE right <-- arr[mid...n]
-      // sort the left side
-      Mergesort(left)
-      // sort the right side
-      Mergesort(right)
-      // merge the sorted left and right sides together
-      Merge(left, right, arr)
-
-ALGORITHM Merge(left, right, arr)
-    DECLARE i <-- 0
-    DECLARE j <-- 0
-    DECLARE k <-- 0
-
-    while i < left.length && j < right.length
-        if left[i] <= right[j]
-            arr[k] <-- left[i]
-            i <-- i + 1
-        else
-            arr[k] <-- right[j]
-            j <-- j + 1
-
-        k <-- k + 1
-
-    if i = left.length
-       set remaining entries in arr to remaining values in right
-    else
-       set remaining entries in arr to remaining values in left
---
+++ javascript/401/Challenge-31/Readme.md
@@ -0,0 +1,12 @@
+# Challenge Summary
+
+- Write a function called repeated word that finds the first word to occur more than once in a string
+- Arguments: string
+- Return: string
+
+## Approach & Efficiency
+O(1) - Time and Space
+
+## Solution
+[Solution/Code](https://github.com/bpfingston/data-structures-and-algorithms/blob/main/javascript/401/Challenge-31/module/hashmaprepeat.js);
+[Test](https://github.com/bpfingston/data-structures-and-algorithms/blob/main/javascript/401/Challenge-26/__Test__/hashmap.test.js)
